8d54e616625dcd8348b6d61cff2d7f6b00ddb9c4
Jarrian <glorianagojar@gmail.com>
2024-11-18 18:29:37 +0800

    feat: Selection Sort Done

---
+++ 10 - 11. Introduction to Algorithms/code/selection_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+void printArray(vector<int> &arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    cout << "Original Array: ";
+    printArray(arr);
+
+    selectionSort(arr);
+
+    cout << "Sorted Array: ";
+    printArray(arr);
+
+    return 0;
+}
